05be9388cf04d80828256e179a4e0e08989f91ec
MMZdev-hub <caiquesousasantos@hotmail.com>
2026-05-18 20:19:38 -0300

    fix: redireciona raiz para login

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Helpdesk</h1>
-      <p>Em construção...</p>
-    </div>
-  );
+    redirect("/login");
 }
